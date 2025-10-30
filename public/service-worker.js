4d5ce05c1e02b531bc856444f1b2d62165647382
Ama-Laundry <edinsonayala1996@gmail.com>
2025-10-31 01:26:50 +0600

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,20 @@
+// public/service-worker.js
+
 importScripts("https://js.pusher.com/beams/service-worker.js");
 
-// Use the imported script
-// This function will be called when a notification is received while the service worker is active
+// This function will be called when a notification is received
 PusherPushNotifications.onNotificationReceived = ({ pushEvent, payload }) => {
-  console.log("Beams Service Worker: Notification Received", payload);
+  console.log("Beams Service Worker: Forcing notification to show", payload);
 
-  // You generally don't need to call showNotification yourself here.
-  // The SDK handles showing the notification based on the payload received
-  // unless you specifically want to customize it *before* it's shown.
-  // If you needed customization, you would use:
-  // const options = { body: payload.notification.body /*, other options */ };
-  // pushEvent.waitUntil(self.registration.showNotification(payload.notification.title, options));
+  // This is the line that forces the notification to show every time
+  const options = {
+    body: payload.notification.body,
+    icon: payload.notification.icon,
+    data: payload.data,
+  };
+  
+  // *** THIS LINE IS NOW UNCOMMENTED AND WILL RUN ***
+  pushEvent.waitUntil(
+    self.registration.showNotification(payload.notification.title, options)
+  );
 };
